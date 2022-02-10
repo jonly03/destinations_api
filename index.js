6b8b54ca9e1546e93e514ba2c2af49e4edc0b3b8
Nelly Sugu <jonly03@gmail.com>
2022-02-10 18:26:57 -0500

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const db = require("./db");
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 // Create an express server (deaf)
 const server = express();
+
+server.use(cors());
 
 // Make the server listen
 server.listen(PORT, () => {
