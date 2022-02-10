aa12972a65028596af782873d5571acbb7fe1029
Nelly Sugu <jonly03@gmail.com>
2022-02-10 16:59:39 -0500

    created GET /

---
+++ db.js
@@ -0,0 +1,13 @@
+const destinations = [
+  {
+    destination: "Eiffel Tower",
+    location: "paris_france",
+  },
+];
+
+const cities = ["LA", "Paris"];
+
+module.exports = {
+  destinations,
+  cities,
+};
---
+++ index.js
@@ -0,0 +1,18 @@
+const db = require("./db");
+const express = require("express");
+
+const PORT = 3000;
+
+// Create an express server (deaf)
+const server = express();
+
+// Make the server listen
+server.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
+
+// Endpoints (Routes) are made up of the method and the path
+// GET /
+server.get("/", (req, res) => {
+  res.send(db);
+});
